2efb81860c42bb0e0168edaf52a2a206da254a96
RodyDev <54083998+RodyDev@users.noreply.github.com>
2019-10-01 17:51:05 -0500

    Update-Coladera.cpp
    Not finished yet

--- Coladera.cpp
+++ Coladera.cpp
@@ -1,1 +1,24 @@
+using namespace std;
 #include <iostream>
+
+int TamanoVector = 1000;
+void MostrarVector( int A[], int Tamano);
+
+int main()
+{
+ int Vector[TamanoVector + 1] = {0,0};
+
+ for (int i = 1;i<= TamanoVector; i++ )
+ {
+ 	Vector[i] = 1;
+ }
+ 
+ 
+ for (int j = 2;j <= TamanoVector; j++)
+ for (int k = 2;k <= (TamanoVector)/j; k++)
+ Vector[k*j] = 0;
+ 
+ MostrarVector(Vector,TamanoVector);
+
+ return 0;
+}
